f968c4bf4ba9916df4108ee220570b81238a11bf
lightbell03 <bellight28@gmail.com>
2023-06-01 17:45:39 +0900

    init

---
+++ src/main/java/com/nhnacademy/team4/mindooray/MindoorayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.team4.mindooray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindoorayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindoorayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/team4/mindooray/MindoorayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.team4.mindooray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindoorayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
